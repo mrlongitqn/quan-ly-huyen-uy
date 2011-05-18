c15722bc2b6182d10ea0627d0ca8b493f070456b
lsytuan@gmail.com <lsytuan@gmail.com@4edf0801-c577-8267-cf21-65b608c799a8>
2011-05-18 06:05:06 +0000

    Add system manager

--- SourceCode/QuanLyHoSoCongChuc/Utils/NhomChucNang.cs
+++ SourceCode/QuanLyHoSoCongChuc/Utils/NhomChucNang.cs
@@ -2,16 +2,90 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml;
 
 namespace QuanLyHoSoCongChuc.Utils
 {
     public class NhomChucNang
     {
+        public static List<string> DanhSachChucNang { get; set; }
+        public static List<ChucNang> DanhSachNhomChucNang { get; set; }
 
-        private class ChucNang
+        public static bool LoadDanhSachChucNang(string xmlPath)
         {
-            public string LoaiNguoiDung { get; set; }
-            public string TenChucNang { get; set; }
+            try
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(xmlPath);
+
+                var lstNode = doc.GetElementsByTagName("tenchucnang");
+                if (lstNode != null)
+                {
+                    DanhSachChucNang = new List<string>();
+                    for (int i = 0; i < lstNode.Count; i++)
+                    {
+                        DanhSachChucNang.Add(lstNode[i].InnerText);
+                    }
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
         }
+
+        public static bool LoadDanhSachChucNangBelongToNguoiDung(string xmlPath)
+        {
+            try
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(xmlPath);
+
+                var lstNode = doc.GetElementsByTagName("loainguoidung");
+                if (lstNode != null)
+                {
+                    DanhSachNhomChucNang = new List<ChucNang>();
+                    for (int i = 0; i < lstNode.Count; i++)
+                    {
+                        var childNodeList = lstNode[i].ChildNodes;
+                        for (int j = 0; j < childNodeList.Count; j++)
+                        {
+                            var chucnang = new ChucNang
+                            {
+                                LoaiNguoiDung = lstNode[i].Attributes["id"].Value,
+                                TenChucNang = childNodeList[j].InnerText
+                            };
+                            DanhSachNhomChucNang.Add(chucnang);
+                        }
+                    }
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static List<ChucNang> LoadDanhSachChucNangByLoaiNguoiDung(string tenloainguoidung)
+        {
+            var lst = new List<ChucNang>();
+            try
+            {
+
+                return lst;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+
+    public class ChucNang
+    {
+        public string LoaiNguoiDung { get; set; }
+        public string TenChucNang { get; set; }
     }
 }
